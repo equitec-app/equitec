6236626d1dafd6bd86821bee2bf17f52c33b8243
edgardoburgos94 <edgardo.burgos94@gmail.com>
2018-08-06 16:04:16 -0500

    Show visit request errors in create view

--- app/controllers/visit_requests_controller.rb
+++ app/controllers/visit_requests_controller.rb
@@ -4,7 +4,20 @@ class VisitRequestsController < ApplicationController
     @visit_request = Headquarter.find(params[:headquarter_id]).visit_requests.new()
   end
 
+  def create
+    @visit_request = Headquarter.find(params[:headquarter_id]).visit_requests.create(visit_requests_params)
+
+  end
+
   def show
     # @visitrequest = VisitRequest.find(params[:id])
+  end
+
+  private
+
+  def visit_requests_params
+    params.require(:visit_request).permit(:air_filter, :battery, :comment, :concept,
+      :coolant, :fuel, :fuel_filter, :mechanical_fail, :money_state, :oil,
+      :oil_filter, :requested_money, :supplie_state, :water_filter, :headquarter_id )
   end
 end
