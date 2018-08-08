44fdc48d193a107788ee99a5a81dd3bf6d8edadf
edgardoburgos94 <edgardo.burgos94@gmail.com>
2018-08-08 12:11:55 -0500

    Add styles to visit request form

--- app/helpers/visit_requests_helper.rb
+++ app/helpers/visit_requests_helper.rb
@@ -1,2 +1,11 @@
 module VisitRequestsHelper
+  def type_of_equipment(equipment)
+    if equipment.class == PowerPlant
+      "Planta electrica"
+    elsif equipment.class == Up
+      "UPS"
+    else
+      "No definido"
+    end
+  end
 end
